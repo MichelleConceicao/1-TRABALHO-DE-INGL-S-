5dbf78124f3b22dce264a58baed8e1cd559de990
MichelleConceição <liliandlentretenimento@gmail.com>
2022-07-01 02:10:09 -0300

    Desafio de inglês

---
+++ script.js
@@ -0,0 +1,30 @@
+let Comedy = [
+    {
+    title:"Oito Mulheres e um Segredo",
+    director:"Gary Ross",
+    year:2018,
+}
+];
+
+let Action = [
+    {
+    title:"Velozes e Furiosos 9",
+    director:"Justin Lin",
+    year:2021,
+    }
+];
+
+let Fantasy = [
+    {
+        title:"Animais Fantásticos: Os Segredos de Dumbledore",
+        director:"David Yates",
+        year:2022,   
+        }
+];
+
+let favoriteMovie= "Harry Potter";
+if(favoriteMovie ==="Harry Potter"){
+    console.log(`MY FAVORIT MOVIE IS"${favoriteMovie}`);
+ }else{
+    console.log(`NOT EVEN 5 STARS"${favoriteMovie}`);
+}
